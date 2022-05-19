0db513b1223812f21595b161eb3499b360ed17c8
IT21482502 <99388775+IT21482502@users.noreply.github.com>
2022-05-19 14:48:20 +0000

    Update

---
+++ RegisteredUser.h
@@ -0,0 +1,11 @@
+#include "Visitor.h"
+
+class RegisteredUser : public Visitor
+{
+protected:
+    char Username [30];
+    char PW [15];
+
+public:
+
+}
---
+++ Visitor.h
@@ -0,0 +1,16 @@
+
+class Visitor
+{
+protected:
+    int userID;
+    char userName [30];
+    char userAddress [100];
+    char userEmail [40];
+    char userPW [15];
+    char Gender;
+    int Phone [10];
+
+public:
+
+}
+
