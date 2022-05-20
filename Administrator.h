f43db0de2098bc61ade0494d6c7e0d6dacbae65b
IT21249334 <99866635+IT21249334@users.noreply.github.com>
2022-05-20 08:44:05 +0000

    IT21249334

--- Administrator.h
+++ Administrator.h
@@ -11,6 +11,7 @@ private :
   Vote* votet[10];
   int AID ;
   string Aname;
+
 public:
   Administrator();
   void setUserDetails(int ID , string name , string userName , char pword[10] , string contctNo);
