c5bcb21a411fd4832720ab5d19ab8b677d7e67d0
IT21482502 <99388775+IT21482502@users.noreply.github.com>
2022-05-20 18:02:03 +0000

    IT21245824

---
+++ DigitalMarketer.cpp
@@ -0,0 +1,38 @@
+#include "Digital_Marketer.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+Digital_Marketer::Digital_Marketer()
+{
+
+};
+
+Digital_Marketer::Digital_Marketer(int pID, string pNAME, const char puserName[], const char pPassword[], string pCntNumber)
+{
+    dmID = pID;
+    dmNAME = pNAME;
+    strcpy(dmuserName, puserName);
+    strcpy(dmPassword, pPassword);
+    dmContactNumber = pCntNumber;
+};
+void Digital_Marketer::DisplaydmDetails()
+{
+    cout << "Digital Marketer ID : " << dmID << endl
+        << "Digital Marketer Name : " << dmNAME << endl
+        << "Username : " << dmuserName << endl
+        << "Password : " << dmPassword << endl
+        << "Contact No : " << dmContactNumber << endl;
+};
+
+void Digital_Marketer::login() {
+
+};
+
+void Digital_Marketer::updateAdvertisements() {
+
+};
+
+Digital_Marketer::~Digital_Marketer() {
+
+};
